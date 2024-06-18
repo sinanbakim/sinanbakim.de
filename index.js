6ee2484dc606dfb2281c20657716a195e8317db2
Sinan Bakim <sinanbakim@googlemail.com>
2024-06-18 22:09:48 +0200

    fix port and package config change

--- index.js
+++ index.js
@@ -3,12 +3,51 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var fs = require('fs');
+var http = require('http');
 var env = require('dotenv').config();
 var app = express();
+
 if (env.error) {
     throw env.error;
 }
-var port = process.env.PORT || 3000;
+var startPort = 3000;
+var endPort = 3100;
+
+function checkIfPortIsAvailable(port) {
+    return new Promise((resolve, reject) => {
+        var server = http.createServer(function (req, res) {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Hello World\n');
+        });
+
+        server.on('error', function (err) {
+            if (err.code === 'EADDRINUSE') {
+                resolve(false);
+            }
+        });
+
+        server.listen(port, function () {
+            server.close();
+            resolve(true);
+        });
+    });
+}
+
+function findAvailablePort(minPort, maxPort) {
+    return new Promise((resolve, reject) => {
+        var port = minPort;
+        var limit = maxPort;
+        var isAvailable = false;
+        while (!isAvailable && port <= limit) {
+            isAvailable = checkIfPortIsAvailable(port);
+            if (isAvailable) {
+                resolve(port);
+            }
+            port++;
+        }
+        resolve(null);
+    });
+}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,6 +61,12 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, function () {
-    console.log('Server is running on port ' + port);
-});
+findAvailablePort(startPort, endPort).then((port) => {
+    if (port) {
+        app.listen(port, function () {
+            console.log('Server started on port ' + port);
+        });
+    } else {
+        console.log('No available port found');
+    }
+}
