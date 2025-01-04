0a9c0868d39f99f2c20e0f3c9c6bbe48ac28f73d
Sinan Bakim <sinanbakim@googlemail.com>
2025-01-04 16:55:22 +0100

    extend

--- server.js
+++ server.js
@@ -4,11 +4,28 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var fs = require('fs');
 var http = require('http');
+
 var env = require('dotenv').config();
 var app = express();
 
+// check if .env file is present
+if (env.error && env.error.code === 'ENOENT') {
+	console.log('No .env file found, using environment variables');
+} else {
+	console.log('Using .env file');
+}
+
+// check if there was an error reading the .env file
 if (env.error) {
 	throw env.error;
+}
+
+// activate logging if needed
+if (process.env.LOGGING === 'true') {
+	app.use(function (req, res, next) {
+		console.log(req.method + ' ' + req.url);
+		next();
+	});
 }
 
 app.use(bodyParser.json());
@@ -21,4 +38,8 @@ app.get('/test', function (req, res) {
 });
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(process.env.PORT, function () {
+	console.log('Server started on port ' + process.env.PORT);
 });
