3a104e2a0086427cb0b4d6aa3eafc1374f3fdd5e
Sinan Bakim <sinanbakim@googlemail.com>
2025-01-04 14:21:20 +0100

    flat

--- index.js
+++ index.js
@@ -8,42 +8,7 @@ var env = require('dotenv').config();
 var app = express();
 
 if (env.error) {
-    throw env.error;
-}
-var startPort = 3000;
-var endPort = 3100;
-
-function checkIfPortIsAvailable(port) {
-    return new Promise((resolve, reject) => {
-        var server = http.createServer(function (req, res) {
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end('Hello World\n');
-        });
-
-        server.on('error', function (err) {
-            server.close(); // Stellen Sie sicher, dass der Server geschlossen wird, auch wenn ein Fehler auftritt
-            if (err.code === 'EADDRINUSE') {
-                resolve(false); // Der Port ist bereits in Gebrauch
-            } else {
-                reject(err); // Ein anderer Fehler tritt auf, lehnen Sie das Promise mit dem Fehler ab
-            }
-        });
-
-        server.listen(port, function () {
-            server.close(); // Schließen Sie den Server, nachdem erfolgreich festgestellt wurde, dass der Port frei ist
-            resolve(true); // Der Port ist verfügbar
-        });
-    });
-}
-
-async function findAvailablePort(minPort, maxPort) {
-    for (let port = minPort; port <= maxPort; port++) {
-        const isAvailable = await checkIfPortIsAvailable(port);
-        if (isAvailable) {
-            return port;
-        }
-    }
-    return null; // Kein verfügbarer Port gefunden
+	throw env.error;
 }
 
 app.use(bodyParser.json());
@@ -52,18 +17,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/test', function (req, res) {
-    res.send('Hello World');
+	res.send('Hello World');
 });
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-findAvailablePort(startPort, endPort).then((port) => {
-    if (port) {
-        app.listen(port, function () {
-            console.log('Server started on port ' + port);
-        });
-    } else {
-        console.log('No available port found');
-    }
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
